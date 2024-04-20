0e5d7e82d757e9207f8fa5400210c144aafbe730
Andy <duyanhnn@outlook.com>
2024-04-20 19:27:27 +0700

    Just commit pls

--- commit.sh
+++ commit.sh
@@ -12,7 +12,9 @@ git remote remove origin &> /dev/null
 git remote add origin https://$GITHUB_TOKEN@github.com/onlyuuuuu/prototypes &> /dev/null
 git branch --set-upstream-to=origin/"$1" "$1" &> /dev/null
 git checkout "$1"
+git stash
 git pull
+git stash pop
 git add $SCRIPT_DIR
 git commit -m "$2"
-git push --set-upstr
+git push --set-upstream origin main &> /dev/null
