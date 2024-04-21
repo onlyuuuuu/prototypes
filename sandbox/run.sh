b560d4f8046cc2640878cfcafd39be9174204189
Andy <duyanhnn@outlook.com>
2024-04-21 17:36:20 +0700

    Auto-generated commit message: /dev/null

--- sandbox/run.sh
+++ sandbox/run.sh
@@ -4,5 +4,5 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 echo "Location of [$(basename "${BASH_SOURCE[0]}")] is: $SCRIPT_DIR"
 
 javac Factorial.java
-jar cvfm MANIFEST.MF factorial.jar *.class
-#java -Xms256m -Xmx512m -Xdebug -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=*:1199 Factorial
+jar --verbose --create --main-class Factorial --file factorial.jar Factorial.class
+java -Xms256m -Xmx512m -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=*:1199 -jar factorial.jar
